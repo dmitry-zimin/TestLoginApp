6f3c469eaa3f865c888e838ddff8d9782f232b29
Dmitry Zimin <dmitry.super.user@gmail.com>
2014-05-29 18:46:13 +0300

    new relation and migration

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,2 +1,5 @@
 class Upload < ActiveRecord::Base
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  belongs_to :user
 end
